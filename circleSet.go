865254bc663e9a27c64a6d779ba1fe1dd7cf283a
jungju <leejungju.go@gmail.com>
2018-04-13 00:37:15 +0900

    앱정보 연동 추가

--- circleSet.go
+++ circleSet.go
@@ -7,14 +7,21 @@ import (
 )
 
 type CircleSet struct {
-	ID          uint         `description:""`
-	CreatedAt   time.Time    `description:"등록일"`
-	UpdatedAt   time.Time    `description:"수정일"`
-	Name        string       `description:"이름"`
-	Description string       `description:"설명" sql:"type:text"`
-	Import      string       `description:""`
-	Units       []CircleUnit `description:""`
-	IsEnable    bool         `description:"사용여부"`
+	ID                    uint         `description:""`
+	CreatedAt             time.Time    `description:"등록일"`
+	UpdatedAt             time.Time    `description:"수정일"`
+	Name                  string       `description:"이름"`
+	Description           string       `description:"설명" sql:"type:text"`
+	Import                string       `description:""`
+	Units                 []CircleUnit `description:""`
+	IsEnable              bool         `description:"사용여부"`
+	AppVersion            string       `description:""`
+	AppTitle              string       `description:""`
+	AppDescription        string       `description:""`
+	AppContact            string       `description:""`
+	AppTermsOfServiceUrl  string       `description:""`
+	AppLicense            string       `description:""`
+	AppSecurityDefinition string       `description:""`
 }
 
 type CircleUnit struct {
